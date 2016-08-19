f9804fe9ae8652664bfd314e495c19039c312783
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-08-19 21:27:10 +0300

    added functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -6,23 +6,14 @@ public class MyFirstProgramm {
         hello("User");
         hello("world");
 
-        double len = 5;
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody + "!");
-    }
-
-    public static double area(double l) {
-        return l * l;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
     }
 }
